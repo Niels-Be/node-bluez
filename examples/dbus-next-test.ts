3c7fc58bf422c40e3427e068c641fba9ac7ac9b5
Niels Becker <aaqqws@googlemail.com>
2019-09-12 12:51:56 +0200

    cleanup

--- examples/dbus-next-test.ts
+++ examples/dbus-next-test.ts
@@ -3,14 +3,21 @@ import * as DBus from 'dbus-next';
 
 const bus = DBus.systemBus();
 
+function delay(delay: number) {
+    return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
 async function run() {
     const bluez = new Bluez({bus: bus});
     await bluez.init();
 
     const adapter = await bluez.getAdapter();
     const devs = await adapter.listDevices();
-    console.log(devs.length);
+    await adapter.StartDiscovery();
+    await delay(3000);
+    console.log("Found: " + devs.length + " devices");
     console.log(await devs[0].getProperties());
+    await adapter.StopDiscovery();
 }
 
 async function run2() {
