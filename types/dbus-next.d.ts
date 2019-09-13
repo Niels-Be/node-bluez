17ed6509cfc2047d8fa3b0005557ce49a662e605
Niels Becker <aaqqws@googlemail.com>
2019-09-13 19:33:08 +0200

    update types

--- types/dbus-next.d.ts
+++ types/dbus-next.d.ts
@@ -1,20 +1,32 @@
-import { EventEmitter } from "events";
 
 declare module 'dbus-next' {
+    import { EventEmitter } from "events";
+    
     export type ObjectPath = string;
+    export type PropertyAccess = "read" | "write" | "readwrite";
+
 
     export namespace interface {
-        interface PropertyOptions {
+        export const ACCESS_READ = 'read';
+        export const ACCESS_WRITE = 'write';
+        export const ACCESS_READWRITE = 'readwrite';
 
+        export interface PropertyOptions {
+            signature: string;
+            access?: PropertyAccess;
+            name?: string;
+            disabled?: boolean;
         }
-        interface MethodOptions {
+        export interface MethodOptions {
             inSignature?: string;
             outSignature?: string;
             name?: string;
             disabled?: boolean;
         }
-        interface SignalOptions {
-
+        export interface SignalOptions {
+            signature: string;
+            name?: string;
+            disabled?: boolean;
         }
 
         export class Interface {
@@ -35,15 +47,31 @@ declare module 'dbus-next' {
         reply?: any;
         constructor(type: string, text: string, reply?: any);
     }
-    export class Message { }
+    export interface Message {
+        serial: number;
+        type: number;
+        flags: number;
+        body?: any;
+        signature?: string;
+        [key: string]: any;
+    }
 
-    export interface MessageBus {
+    export class MessageBus {
         getProxyObject(name: string, path: string): Promise<ProxyObject>;
         getProxyObject(name: string, path: string, xml: string): Promise<ProxyObject>;
         disconnect(): void;
 
         export(path: ObjectPath, interface: interface.Interface): void;
         unexport(path: ObjectPath, interface: interface.Interface): void;
+
+        requestName(name: string, flags: number): Promise<number>;
+        releaseName(name: string): Promise<number>;
+
+        newSerial(): number;
+        addMethodHandler(handler: Function): void;
+        removeMethodHandler(handler: Function): void;
+        call(msg: Message): Promise<Message | null>;
+        send(msg: Message): void;
     }
     export interface ProxyObject {
         bus: MessageBus;
