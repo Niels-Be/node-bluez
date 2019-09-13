acf548e49eb2eed54b83ba3c789712202d891284
Niels Becker <aaqqws@googlemail.com>
2019-09-13 19:14:01 +0200

    add serial profile

--- src/profile.ts
+++ src/profile.ts
@@ -1,8 +1,85 @@
 import { Device } from "./device";
 
+export interface ProfileOptions {
+    /*
+        string Name
+            Human readable name for the profile
+    */
+    Name: string;
+    /*
+        string Service
+            The primary service class UUID
+            (if different from the actual
+                profile UUID)
+    */
+    Service: string;
+    /*
+        string Role
+            For asymmetric profiles that do not
+            have UUIDs available to uniquely
+            identify each side this
+            parameter allows specifying the
+            precise local role.
+            Possible values: "client", "server"
+    */
+    Role: string;
+    /*
+        uint16 Channel
+            RFCOMM channel number that is used
+            for client and server UUIDs.
+            If applicable it will be used in the
+            SDP record as well.
+    */
+    Channel: number;
+    /*
+        uint16 PSM
+            PSM number that is used for client
+            and server UUIDs.
+            If applicable it will be used in the
+            SDP record as well.
+    */
+    PSM: number;
+    /*
+        boolean RequireAuthentication
+            Pairing is required before connections
+            will be established. No devices will
+            be connected if not paired.
+    */
+    RequireAuthentication: boolean;
+    /*
+        boolean RequireAuthorization
+            Request authorization before any
+            connection will be established.
+    */
+    RequireAuthorization: boolean;
+    /*
+        boolean AutoConnect
+            In case of a client UUID this will
+            force connection of the RFCOMM or
+            L2CAP channels when a remote device
+            is connected.
+    */
+    AutoConnect: boolean;
+    /*
+        string ServiceRecord
+            Provide a manual SDP record.
+    */
+    ServiceRecord: string;
+    /*
+        uint16 Version
+            Profile version (for SDP record)
+    */
+    Version: number;
+    /*
+        uint16 Features
+            Profile features (for SDP record)
+    */
+    Features: number;
+}
 
 export interface Profile {
-    readonly ProfileOptions: {[name: string]: any};
+
+    readonly ProfileOptions: Partial<ProfileOptions>;
     readonly UUID: string;
 
     /*
---
+++ src/serialProfile.ts
@@ -0,0 +1,20 @@
+import { Profile } from "./profile";
+import { Device } from "./device";
+
+export class SerialProfile implements Profile {
+    ProfileOptions = {
+        Name: "Node Serial Port",
+        Role: "client",
+        PSM: 0x0003
+    };
+    // Bluetooth SSP uuid
+    UUID = "00001101-0000-1000-8000-00805f9b34fb";
+
+    NewConnection(device: Device, fd: number, options: { [name: string]: any; }): void | Promise<void> {
+        // Get a socket from the RFCOMM FD
+        //const socket = new BluetoothSocket(fd);
+        // the socket is a standard nodejs duplex stream
+        //socket.on("data", (data: any) => console.log(data));
+        //socket.write("Hello World");
+    }
+}
